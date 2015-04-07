0c0c61f13531f1faaddc3acf188320b29bb363e6
Timan Rebel <timan@trebel.nl>
2015-04-07 13:03:58 +0200

    Upgraded inapp purchases

--- android/InApp.js
+++ android/InApp.js
@@ -1,35 +1,34 @@
 var Alloy = require('alloy'),
 	_ = Alloy._,
 	Backbone = Alloy.Backbone,
-	Acl = require('RebelFrame/Acl'),
 	InAppBilling = require('ti.inappbilling');
 
 /**
- * @class InApp
- *
- * In-app purchases support for Android
- */
+* @class InApp
+*
+* In-app purchases support for Android
+*/
 var InApp = {
 	getReceipt: function(callback) {
 		Ti.API.error('Android In-App Billing does not have receipts.');
 	},
 
 	/*
-	 * Make an in-app purchase
-	 *
-	 * @param {Object} product Product to purchase
-	 * @param {String} product.id the id of the product to be purchased
-	 * @param {String} [product.type] Accepted product types:
-	 * 		'in-app' - Standard in-app product (default if no productType specified).
-	 * 		'subscription' - Recurring monthly or annual billing product.
-	 * @param {String} [product.developerPayload] A developer-specified string that contains supplemental information about an order.
-	 */
+	* Make an in-app purchase
+	*
+	* @param {Object} product Product to purchase
+	* @param {String} product.id the id of the product to be purchased
+	* @param {String} [product.type] Accepted product types:
+	* 		'in-app' - Standard in-app product (default if no productType specified).
+	* 		'subscription' - Recurring monthly or annual billing product.
+	* @param {String} [product.developerPayload] A developer-specified string that contains supplemental information about an order.
+	*/
 	purchase: function(product, callback) {
 		_successCallback = callback;
 
-		var response = InAppBilling.requestPurchase({
+		var response = InAppBilling.purchase({
 			productId: product.id,
-			productType: product.type == 'subscription' ? InAppBilling.ITEM_TYPE_SUBSCRIPTION : InAppBilling.ITEM_TYPE_INAPP,
+			type: product.type == 'subscription' ? InAppBilling.ITEM_TYPE_SUBSCRIPTION : InAppBilling.ITEM_TYPE_INAPP,
 			developerPayload: product.developerPayload
 		});
 
@@ -47,9 +46,11 @@ var InApp = {
 	},
 
 	isSupported: function() {
-		var response = InAppBilling.checkBillingSupported(InAppBilling.ITEM_TYPE_SUBSCRIPTION);
+		return true;
 
-		Ti.API.error(JSON.stringify(response, null, 4));
+		// var response = InAppBilling.checkBillingSupported(InAppBilling.ITEM_TYPE_SUBSCRIPTION);
+		//
+		// Ti.API.error(JSON.stringify(response, null, 4));
 	}
 };
 
@@ -59,101 +60,185 @@ _.extend(InApp, Backbone.Events);
 var _successCallback;
 
 /**
- * To use the Signature Verification code included in the module, you must set the Public Key. If this is not set, the process will function but will not internally verify any Market responses.
- */
-InAppBilling.setPublicKey(Alloy.CFG.inAppBillingPublicKey);
+* Initializes the Billing Service which will be used to send messages to the Android Market. When the Billing Service is bound to the Market Service, an ON_BIND_EVENT e is fired. When the Billing Service connects to the Market service, an ON_CONNECT_EVENT is fired, and the Billing Service can now be used for other requests. This MUST be called before any of the 5 request functions.
+*/
+InAppBilling.addEventListener('setupcomplete', onConnect);
+InAppBilling.addEventListener('purchasecomplete', onPurchaseComplete);
+InAppBilling.startSetup({
+	publicKey: Alloy.CFG.inAppBillingPublicKey,
+	debug: true
+});
 
-/**
- * Initializes the Billing Service which will be used to send messages to the Android Market. When the Billing Service is bound to the Market Service, an ON_BIND_EVENT e is fired. When the Billing Service connects to the Market service, an ON_CONNECT_EVENT is fired, and the Billing Service can now be used for other requests. This MUST be called before any of the 5 request functions.
- */
-InAppBilling.addEventListener(InAppBilling.ON_BIND_EVENT, onBind);
-InAppBilling.addEventListener(InAppBilling.ON_CONNECT_EVENT, onConnect);
-InAppBilling.addEventListener(InAppBilling.PURCHASE_STATE_CHANGED_EVENT, onTransactionState);
-InAppBilling.addEventListener(InAppBilling.NOTIFY_EVENT, onNotify);
-InAppBilling.startBillingService();
-
-function markProductAsPurchased(order) {
-	Ti.API.error(JSON.stringify(order, null, 4));
+function markProductAsPurchased(purchase) {
+	console.log('markProductAsPurchased');
+	Ti.API.error(JSON.stringify(purchase, null, 4));
 
 	if (_successCallback)
-		_successCallback();
+		_successCallback({
+			id: purchase.productId,
+			token: purchase.token
+		});
 }
 
 /**
- * Handle `ON_BIND_EVENT` event. The event is fired when the Billing Service binds with the Market service
- *
- * @param {Object} evt Event details
- * @param {Number} evt.result Result of the binding, equal to one of the following: InAppBilling.SUCCESS, InAppBilling.FAILED, InAppBilling.SECEXCEPTION
- */
-function onBind(evt) {
-	if (evt.result == InAppBilling.SUCCESS)
-		Ti.API.error('Binding was successful');
-	else if (evt.result == InAppBilling.FAILED)
-		Ti.API.error('Binding failed');
-	else if (evt.result == InAppBilling.SECEXCEPTION)
-		Ti.API.error('Binding failed, a SECEXCEPTION occured');
+* Handle `ON_CONNECT_EVENT` event. The event is fired when the Billing Service connects to the market. This event has no properties, but signifies that requests can now be made to the Android Market.
+*/
+function onConnect(evt) {
+	console.log('Setup response: ' + responseString(evt.responseCode));
+
+	if (evt.success) {
+		console.log('Setup completed successfully!');
+
+		InApp.trigger('ready');
+	} else {
+		console.log('Setup FAILED.');
+	}
 }
 
 /**
- * Handle `ON_CONNECT_EVENT` event. The event is fired when the Billing Service connects to the market. This event has no properties, but signifies that requests can now be made to the Android Market.
- */
-function onConnect() {
-	Ti.API.error('onConnect');
+* Handle `purchasecomplete` event. The event is fired when the module receives an asynchronous purchasecomplete response from the market.
 
-	InApp.trigger('ready');
-}
-
-/**
- * Handle `PURCHASE_STATE_CHANGED_EVENT` event. The event is fired when the module receives an asynchronous PURCHASE_STATE_CHANGED response from the market.
- *
- * @param {Object} evt Event detais
- * @oaram {String} evt.signedData JSON String which can be parsed into a JSON object (see Android documentation: http://developer.android.com/guide/market/billing/billing_reference.html#billing-intents).
- * @param {String} evt.signature this can be used to verify the response received. If the Public Key has been set, the module will verify automatically
- * @param {Number} evt.responseCode Response with one the following possible values:
- 
- * InAppBilling.SIGNATURE_VERIFIED
- * InAppBilling.NULL_DATA
- * InAppBilling.SIGNATURE_ERROR
- * InAppBilling.UNKNOWN_NONCE
- * InAppBilling.PUBLIC_KEY_NULL
- *
- * @return {[type]} [description]
- */
-function onTransactionState(evt) {
-	Ti.API.error('onTransactionState');
+*/
+function onPurchaseComplete(evt) {
+	console.log('onPurchaseComplete');
+	console.log('Purchase response: ' + responseString(evt.responseCode));
 	Ti.API.error(JSON.stringify(evt, null, 4));
 
-	if (evt.result == InAppBilling.RESULT_OK) {
-		if (evt.signedData) {
-			var response = JSON.parse(evt.signedData);
+	if (evt.responseCode == InAppBilling.RESULT_OK) {
+		markProductAsPurchased(evt.purchase);
+	} else if (evt.responseCode == InAppBilling.RESULT_ITEM_ALREADY_OWNED) {
+		_successCallback({
+			alreadyOwned: true
+		});
+	} else
+		alert(responseString(evt.responseCode));
 
-			Ti.API.error('response: ' + JSON.stringify(response, null, 4));
+	// if(evt.result == InAppBilling.RESULT_OK) {
+	// 	if (evt.signedData) {
+	// 		var response = JSON.parse(evt.signedData);
+	//
+	// 		Ti.API.error('response: ' + JSON.stringify(response, null, 4));
+	//
+	// 		/*
+	// 		 * We are not guaranteed to have any orders returned so
+	// 		 * we need to make sure that this one exists before using it.
+	// 		 *
+	// 		 * If there is no notificationId then there is no need to confirmNotifications().
+	// 		 * This happens when restoreTransactions() triggers a PURCHASE_STATE_CHANGED_EVENT.
+	// 		 */
+	// 		if(response.orders.length) {
+	// 			_.each(response.orders, function(order) {
+	// 				markProductAsPurchased(order);
+	// 			});
+	// 		}
+	// 	}
+	// }
+}
 
-			/*
-			 * We are not guaranteed to have any orders returned so
-			 * we need to make sure that this one exists before using it.
-			 *
-			 * If there is no notificationId then there is no need to confirmNotifications().
-			 * This happens when restoreTransactions() triggers a PURCHASE_STATE_CHANGED_EVENT.
-			 */
-			if (response.orders.length) {
-				_.each(response.orders, function(order) {
-					markProductAsPurchased(order);
-				});
+function onRequestProducts(e) {
+	console.log('Query Inventory response: ' + responseString(e.responseCode));
+	var inventory = e.inventory;
+	var purchaseIds = ['gas', 'infinite_gas'];
+	var purchase, details;
+	if (e.success) {
+		for (var i = 0, j = purchaseIds.length; i < j; i++) {
+			// Check for details
+			if (inventory.hasDetails(purchaseIds[i])) {
+				console.log('Check log for Purchase ' + i + ' details');
+				Ti.API.info('Details: ' + JSON.stringify(inventory.getDetails(purchaseIds[i])));
+			}
+			// Check for purchase
+			if (inventory.hasPurchase(purchaseIds[i])) {
+				purchase = inventory.getPurchase(purchaseIds[i]);
+				// Print details for each purchase
+				console.log('Check log for Purchase ' + i + ' properties');
+				Ti.API.info(purchaseProperties(purchase));
+
+				// Queue 'gas' up to be consumed if it is owned
+				if (purchase.productId === 'gas' &&
+					purchase.purchaseState === InAppBilling.PURCHASE_STATE_PURCHASED) {
+					toConsume = purchase;
+					console.log('gas is queued to be consumed');
+				}
 			}
 		}
 	}
 }
 
-function onNotify(evt) {
+module.exports = InApp;
 
-	// if(evt.responseCode == InAppBilling.RESULT_OK) {
-	var response = InAppBilling.getPurchaseInformation({
-		notificationIds: [evt.notifyId]
-	});
+function responseString(responseCode) {
+	switch (responseCode) {
+		case InAppBilling.RESULT_OK:
+			return 'OK';
+		case InAppBilling.RESULT_USER_CANCELED:
+			return 'USER CANCELED';
+		case InAppBilling.RESULT_BILLING_UNAVAILABLE:
+			return 'BILLING UNAVAILABLE';
+		case InAppBilling.RESULT_ITEM_UNAVAILABLE:
+			return 'ITEM UNAVAILABLE';
+		case InAppBilling.RESULT_DEVELOPER_ERROR:
+			return 'DEVELOPER ERROR';
+		case InAppBilling.RESULT_ERROR:
+			return 'RESULT ERROR';
+		case InAppBilling.RESULT_ITEM_ALREADY_OWNED:
+			return 'RESULT ITEM ALREADY OWNED';
+		case InAppBilling.RESULT_ITEM_NOT_OWNED:
+			return 'RESULT ITEM NOT OWNED';
 
-	Ti.API.error('getPurchaseInformation() response: ' + JSON.stringify(response, null, 4));
-	// }
+		case InAppBilling.IAB_RESULT_REMOTE_EXCEPTION:
+			return 'IAB RESULT REMOTE EXCEPTION';
+		case InAppBilling.IAB_RESULT_BAD_RESPONSE:
+			return 'IAB RESULT BAD RESPONSE';
+		case InAppBilling.IAB_RESULT_VERIFICATION_FAILED:
+			return 'IAB RESULT VERIFICATION FAILED';
+		case InAppBilling.IAB_RESULT_SEND_INTENT_FAILED:
+			return 'IAB RESULT SEND INTENT FAILED';
+		case InAppBilling.IAB_RESULT_UNKNOWN_PURCHASE_RESPONSE:
+			return 'IAB RESULT UNKNOWN PURCHASE RESPONSE';
+		case InAppBilling.IAB_RESULT_MISSING_TOKEN:
+			return 'IAB RESULT MISSING TOKEN';
+		case InAppBilling.IAB_RESULT_UNKNOWN_ERROR:
+			return 'IAB RESULT UNKNOWN ERROR';
+		case InAppBilling.IAB_RESULT_SUBSCRIPTIONS_NOT_AVAILABLE:
+			return 'IAB RESULT SUBSCRIPTIONS NOT AVAILABLE';
+		case InAppBilling.IAB_RESULT_INVALID_CONSUMPTION:
+			return 'IAB RESULT INVALID CONSUMPTION';
+	}
+	return '';
 }
 
-module.exports = InApp;
+function purchaseStateString(state) {
+	switch (state) {
+		case InAppBilling.PURCHASE_STATE_PURCHASED:
+			return 'PURCHASE STATE PURCHASED';
+		case InAppBilling.PURCHASE_STATE_CANCELED:
+			return 'PURCHASE STATE CANCELED';
+		case InAppBilling.PURCHASE_STATE_REFUNDED:
+			return 'PURCHASE STATE REFUNDED';
+	}
+	return '';
+}
+
+function purchaseTypeString(state) {
+	switch (state) {
+		case InAppBilling.ITEM_TYPE_INAPP:
+			return 'ITEM TYPE INAPP';
+		case InAppBilling.ITEM_TYPE_SUBSCRIPTION:
+			return 'ITEM TYPE SUBSCRIPTION';
+	}
+	return '';
+}
+
+function purchaseProperties(p) {
+	var str = 'type: ' + purchaseTypeString(p.type) +
+		'\norderId: ' + p.orderId +
+		'\npackageName: ' + p.packageName +
+		'\nproductId: ' + p.productId +
+		'\npurchaseTime: ' + new Date(p.purchaseTime) +
+		'\npurchaseState: ' + purchaseStateString(p.purchaseState) +
+		'\ndeveloperPayload: ' + p.developerPayload +
+		'\ntoken: ' + p.token;
+
+	return str;
+}
--- iphone/InApp.js
+++ iphone/InApp.js
@@ -11,25 +11,25 @@ var Alloy = require('alloy'),
  */
 var InApp = {
 	getReceipt: function(callback) {
-		// function validate() {
-		// 	Ti.API.error('Validating receipt.');
-		// 	Ti.API.error('Receipt is Valid: ' + Storekit.validateReceipt());
+		function validate() {
+			Ti.API.error('Validating receipt.');
+			Ti.API.error('Receipt is Valid: ' + Storekit.validateReceipt());
 
-		// 	callback(Storekit.receipt);
-		// }
+			callback(Storekit.receipt);
+		}
 
 		//  During development it is possible that the receipt does not exist.
 		//  This can be resolved by refreshing the receipt.
 
-		// if (!Storekit.receiptExists) {
-		// 	Ti.API.error('Receipt does not exist yet. Refreshing to get one.');
-		// 	Storekit.refreshReceipt(null, function() {
-		// 		validate();
-		// 	});
-		// } else {
-		// 	Ti.API.error('Receipt does exist.');
-		// 	validate();
-		// }
+		if (!Storekit.receiptExists) {
+			Ti.API.error('Receipt does not exist yet. Refreshing to get one.');
+			Storekit.refreshReceipt(null, function() {
+				validate();
+			});
+		} else {
+			Ti.API.error('Receipt does exist.');
+			validate();
+		}
 	},
 
 	purchase: function(product, callback) {
@@ -40,9 +40,9 @@ var InApp = {
 		// }
 		Storekit.purchase({
 			product: product,
-			// applicationUsername is a opaque identifier for the user’s account on your system. 
+			// applicationUsername is a opaque identifier for the user’s account on your system.
 			// Used by Apple to detect irregular activity. Should hash the username before setting.
-			applicationUsername: Ti.Utils.sha1(Acl.getLoggedinUser().id + Acl.salt)
+			applicationUsername: Acl.isLoggedin() ? Ti.Utils.sha1(Acl.getLoggedinUser().id + Acl.salt) : null
 		});
 	},
 
@@ -54,7 +54,9 @@ var InApp = {
 		});
 	},
 
-	restore: function() {
+	restore: function(callback) {
+		_successCallback = callback;
+		
 		Storekit.restoreCompletedTransactions();
 	},
 
@@ -74,14 +76,14 @@ var _successCallback;
  against the "Sandbox" or "Live" server. If you are verifying auto-renewable subscriptions then you need
  to set the shared secret for the application from your iTunes Connect account.
  */
-Storekit.receiptVerificationSandbox = (Ti.App.deployType !== 'production');
+Storekit.receiptVerificationSandbox = true;//(Ti.App.deployType !== 'production');
 Storekit.receiptVerificationSharedSecret = Alloy.CFG.storeKitReceiptVerificationSharedSecret;
 
 /*
  autoFinishTransactions must be disabled (false) in order to start Apple hosted downloads.
  If autoFinishTransactions is disabled, it is up to you to finish the transactions.
  Transactions must be finished! Failing to finish transactions will cause your app to run slowly.
- Finishing a transaction at any time before its associated download is complete will cancel the download. 
+ Finishing a transaction at any time before its associated download is complete will cancel the download.
  */
 Storekit.autoFinishTransactions = true;
 
@@ -89,7 +91,7 @@ Storekit.autoFinishTransactions = true;
  bundleVersion and bundleIdentifier must be set before calling validateReceipt().
  Do not pull these values from the app, they should be hard coded for security reasons.
  */
-Storekit.bundleVersion = Ti.App.version; // eg. "1.0.0"
+Storekit.bundleVersion = '1.0.2';//Ti.App.version; // eg. "1.0.0"
 Storekit.bundleIdentifier = Ti.App.id; // eg. "com.appc.storekit"
 
 /**
@@ -241,21 +243,42 @@ function onRestoredCompletedTransactions(evt) {
 				Ti.API.error('Restored Receipt is Invalid.');
 			}
 		}
-		for (var i = 0; i < evt.transactions.length; i++) {
-			if (!IOS7 && verifyingReceipts) {
-				Storekit.verifyReceipt(evt.transactions[i], function(e) {
-					if (e.valid) {
-						markProductAsPurchased(e.productIdentifier);
-					} else {
-						Ti.API.error("Restored transaction is not valid");
-					}
-				});
-			} else {
-				markProductAsPurchased(evt.transactions[i].productIdentifier);
+		if(evt.transactions) {
+			for (var i = 0; i < evt.transactions.length; i++) {
+				if (!IOS7 && verifyingReceipts) {
+					Storekit.verifyReceipt(evt.transactions[i], function(e) {
+						if (e.valid) {
+							markProductAsPurchased(e.productIdentifier);
+						} else {
+							Ti.API.error("Restored transaction is not valid");
+						}
+					});
+				} else {
+					markProductAsPurchased(evt.transactions[i].productIdentifier);
+				}
 			}
+			alert('Restored ' + evt.transactions.length + ' purchases!');
 		}
-		alert('Restored ' + evt.transactions.length + ' purchases!');
 	}
 }
+
+/**
+ * Tells us if the version of iOS we are running on is iOS 7 or later
+ */
+function isIOS7Plus() {
+	if (Titanium.Platform.name == 'iPhone OS') {
+		var version = Titanium.Platform.version.split(".");
+		var major = parseInt(version[0], 10);
+
+		// can only test this support on a 3.2+ device
+		if (major >= 7) {
+			return true;
+		}
+	}
+	return false;
+
+}
+
+var IOS7 = isIOS7Plus();
 
 module.exports = InApp;
